dea807d27ec34243fcf2ef14db90dbfac7f42a28
Fevzi Anifieiev <fevzi.anifyeyev@gmail.com>
2020-02-24 19:01:07 -0500

    (#10) Fixed setup3

--- pyeo/__init__.py
+++ pyeo/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for pyeo."""
 
-__author__ = """Fevzi Anifieiev"""
+__author__ = 'Fevzi Anifieiev'
 __email__ = 'fevzi.anifyeyev@gmail.com'
 __version__ = '0.1.0'
+__licence__ = 'MIT'
